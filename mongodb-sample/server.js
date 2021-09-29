51f119fe7844a9a901a8bb758e3c31c4510e0326
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-09-30 00:05:11 +0530

    mongodb database query app

---
+++ mongodb-sample/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const connectionString =
+  process.env.MONGODB_CONNECTION_STRING || "mongodb://localhost:27017";
+
+async function init() {
+  const client = new MongoClient(connectionString, {
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+
+  const app = express();
+
+  app.get("/get", async (req, res) => {
+    const db = await client.db("adoption");
+    const collection = db.collection("pets");
+
+    const pets = collection
+      .find(
+        {
+          $text: { $search: req.query.search },
+        },
+        { _id: false }
+      )
+      .sort({ score: { $meta: "textScore" } })
+      .limit(10)
+      .toArray();
+
+    res.json({ status: "ok", pets }).end();
+  });
+
+  const PORT = 3000;
+  app.use(express.static("./static"));
+  app.listen(PORT);
+  console.log(`running on http://localhost:${PORT}`);
+}
+init();
